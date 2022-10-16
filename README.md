f79d8f6b3e72552df4a9739a728403cca7b55c02
samuelgarro <samuelgarro@gmail.com>
2022-10-15 23:21:32 -0300

    se elimina cache

--- README.md
+++
@@ -1,2 +0,0 @@
-# project-api
-This project, is a project for university for progamming web of UNVIME.
--- src/main/java/com/unvime/projectapi/repository/IProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unvime.projectapi.repository;
-
-import com.unvime.projectapi.models.entity.Project;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IProjectRepository extends MongoRepository<Project, Long> {
-}
