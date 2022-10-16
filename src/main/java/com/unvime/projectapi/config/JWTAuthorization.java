81d053aa0ad9b2ecae6e419ecd0f92c3d770a721
samuelgarro <samuelgarro@gmail.com>
2022-10-16 13:53:11 -0300

    se crea configuracion de autorizacion por jwt

---
+++ src/main/java/com/unvime/projectapi/config/JWTAuthorization.java
@@ -0,0 +1,66 @@
+package com.unvime.projectapi.config;
+
+import io.jsonwebtoken.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JWTAuthorization extends OncePerRequestFilter {
+    private final String HEADER = "Authorization";
+    private final String PREFIX = "Bearer ";
+    private final String SECRET = "miSecretKey";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            if (existToken(request)) {
+                Claims claims = validateToken(request);
+                if (claims.get("authorities") != null) {
+                    setAuthentication(claims);
+                } else {
+                    SecurityContextHolder.clearContext();
+                }
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.sendError(HttpStatus.FORBIDDEN.value(), e.getMessage());
+        }
+    }
+
+    private boolean existToken(HttpServletRequest request) {
+        String authenticationHeader = request.getHeader(HEADER);
+        return authenticationHeader != null && authenticationHeader.startsWith(PREFIX);
+    }
+
+    private Claims validateToken(HttpServletRequest request) {
+        String token = request.getHeader(HEADER).replace(PREFIX, "");
+        return Jwts.parser()
+                .setSigningKey(SECRET.getBytes())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private void setAuthentication(Claims claims) {
+        List<String> authorities = (List<String>) claims.get("authorities");
+
+        var list = authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null, list);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+}
